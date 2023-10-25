b24a24501cac931c6093f04fc682164ec706e708
Corey-Haro <146971260+Corey-Haro@users.noreply.github.com>
2023-10-25 10:47:10 -0700

    phtoto

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -3,19 +3,16 @@
 // Date: 10/ 25 /2023
 
 // Define Varibables
-
 make = "Toyota"
 model = "Tundra"
 color = "White"
 year= "2016"
 
 // Calculate
-
 age = 2023 - year
 
 // Outputs
-
-document.writeln("Make: " + make + " <br")
-document.writeln("Model: " + model + " <br")
-document.writeln("Color: " + color + " <br")
-document.writeln("Age:" + make + " years<br")
+document.writeln("Make: " + make + " <br>")
+document.writeln("Model: " + model + "<br>")
+document.writeln("Color: " + color + " <br>")
+document.writeln("Age: " + age + " years<br>")
