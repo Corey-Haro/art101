29c0b303d0c0987b39d0b63648a65421e5cd0aa4
Corey-Haro <146971260+Corey-Haro@users.noreply.github.com>
2023-10-27 09:50:31 -0700

    truck info

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -3,7 +3,7 @@
 // Date: 10/27/2023
 
 // Define Variables
-myTransport = ["Toyota Tundra", "BCycle Electric Bike", "Carpool", "Walking"]
+myTransport = ["Toyota Tundra", " BCycle Electric Bike", " Carpool", " Walking"];
 
 // Object for my Dads Vehicle 
 myMainRide = {
@@ -14,12 +14,11 @@ myMainRide = {
   age: function() {
     return 2023 - this.year; 
   }
-
 }
 
 
 // Output
-document.writeln("Methods for My 1 Hour Commute to Campus: ", myTransport, "</br>")
+document.writeln("Methods for My 1 Hour Commute to Campus: ", myTransport, "</br>");
 // How to write n object into the document- code from canvas page (and help from cella) Lab 6 Wes Modes
 document.writeln("My Dads Truck: <pre>",
     JSON.stringify(myMainRide, null,'\t'), "</pre>");
