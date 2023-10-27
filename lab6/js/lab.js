ef45828187a89106838cf18b2feca0d38137d5bb
Corey-Haro <146971260+Corey-Haro@users.noreply.github.com>
2023-10-27 08:49:08 -0700

    java script code

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Corey Haro
+// Date: 10/27/2023
 
-// Constants
+// Define Variables
+myTransport = ["Toyota Tundra", "BCycle Electric Bike", "Carpool", "Walking"]
 
-// Functions
+// Object for my Dads Vehicle 
+myMainRide = {
+  make: "Toyota",
+  model: "Tundra",
+  color: "White", 
+  year: 2016,
+  age: function() {
+    return 2023 - this.year; 
+  }
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+// Output
+document.writeln("Methods for My 1 Hour Commute to Campus: ", myTransport, "</br>")
+// How to write n object into the document- code from canvas page (and help from cella) Lab 6 Wes Modes
+document.writeln("My Dads Truck: <pre>",
+    JSON.stringify(myMainRide, null,'\t'), "</pre>");
+
