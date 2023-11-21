429683fd2e565f6ac34c064d5aa68d11bc8ec4bc
Corey-Haro <146971260+Corey-Haro@users.noreply.github.com>
2023-11-21 00:55:44 -0800

    nnnn

--- Lab11/js/lab11.js
+++ Lab11/js/lab11.js
@@ -4,39 +4,40 @@
 // code credit to Wes Modes
 
 // Sorts the charchters of a string in alphabetical order. 
-function sortString(inputString){
-// we haev to convert our
-// string to an array and back again to sort it
-return inputString.split('').sort().join('');
+function sortString(inputString) {
+    // string to an array and back again to sort it
+    return inputString.split('').sort().join('');
 }
+
 // Click listener for button
 $("#submit-name").click(function(){
-
-
-//get value of input 
-const userName = $("#user-name").val();
- userNameSorted = sortString(userName);
-
-//aappend a new div to our output div
-$("#output").html('<div class="text">' + userNameSorted + '</p></div>');
+    //get value of input 
+    const userName = $("#user-name").val();
+    //sorting
+    userNameSorted = sortString(userName);
+    //aappend a new div to our output div
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
 });
 
+
+
 $("#submit-color").click(function(){
-    const useColor = $("#fav-color").val();
-    $("#output").append('<div class="color"><p> Your favorite color: ' + useColor + '</p></div>');
+    const userColor = $("#fav-color").val();
+    $("#output").html('<div class="color"><p> Your favorite color: ' + userColor + '</p></div>');
 
 });
 
 
 $("#submit-birth").click(function(){
-    const useBirthDay = $("#birth-day").val();
-    $("#output").append('<div class="date"><p> Your birthday: ' + useBirthDay + '</p></div>');
+    const userBirthDay = $("#birth-day").val();
+    $("#output").html('<div class="date"><p> Your birthday: ' + userBirthDay + '</p></div>');
 
 });
 
 
 $("#submit-animal").click(function(){
-    const useAnimal = $("#fav-animal").val();
-    $("#output").append('<div class="animal"><p> Your favorite animal: ' + useAnimal + '</p></div>');
+    const userAnimal = $("#fav-animal").val();
+    $("#output").html('<div class="animal"><p> Your favorite animal: ' + userAnimal + '</p></div>');
 
 });
+
