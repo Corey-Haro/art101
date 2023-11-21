55ff524cc7dd2d8fc9ed4d73d507fd91eab81904
Corey-Haro <146971260+Corey-Haro@users.noreply.github.com>
2023-11-21 12:13:09 -0800

    nn

--- Lab11/js/lab11.js
+++ Lab11/js/lab11.js
@@ -10,34 +10,34 @@ function sortString(inputString) {
 }
 
 // Click listener for button
-$("#submit-name").click(function(){
+$("#submit-name").click(function () {
     //get value of input 
     const userName = $("#user-name").val();
     //sorting
     userNameSorted = sortString(userName);
     //aappend a new div to our output div
-    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+    $("#output").append('<div class="text"><p> Your Name:' + userNameSorted + '</p></div>');
 });
 
 
 
-$("#submit-color").click(function(){
+$("#submit-color").click(function () {
     const userColor = $("#fav-color").val();
-    $("#output").html('<div class="color"><p> Your favorite color: ' + userColor + '</p></div>');
+    $("#output").append('<div class="color"><p> Your favorite color: ' + userColor + '</p></div>');
 
 });
 
 
-$("#submit-birth").click(function(){
+$("#submit-birth").click(function () {
     const userBirthDay = $("#birth-day").val();
-    $("#output").html('<div class="date"><p> Your birthday: ' + userBirthDay + '</p></div>');
+    $("#output").append('<div class="date"><p> Your birthday: ' + userBirthDay + '</p></div>');
 
 });
 
 
-$("#submit-animal").click(function(){
+$("#submit-animal").click(function () {
     const userAnimal = $("#fav-animal").val();
-    $("#output").html('<div class="animal"><p> Your favorite animal: ' + userAnimal + '</p></div>');
+    $("#output").append('<div class="animal"><p> Your favorite animal: ' + userAnimal + '</p></div>');
 
 });
 
