02bc57f924fafaae43dc453002ae3adc5762a8d5
Corey-Haro <146971260+Corey-Haro@users.noreply.github.com>
2023-11-26 21:45:18 -0800

    lab 13

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -7,17 +7,17 @@ var maxFactors = 4;
 
 var outputEl = $("#output");
 
-// get the values from the webpage and write them in an object 
+// get the values from the webpage and write them in an object
 function getFactorObj() {
     var factorObj = {};
-    for (var factor=0; factor<maxFactors; factor++){
+    for (var factor=0; factor<maxFactors; factor++) {
         var numId = "num" + factor;
         var textId = "text" + factor;
         var numValue = $("#" + numId).val();
-        var textValue = $("#" + textId).val();
+        var textValue = $("#" +textId).val();
         console.log(factor + ") num:", numValue, "text:", textValue);
-        //if either value is blank dont use it
-        if (numValue && textValue){
+        // If either value is blank, do not use it 
+        if (numValue && textValue) {
             factorObj[numValue] = textValue;
         }
     }
@@ -29,39 +29,38 @@ function outputToPage(str) {
     outputEl.append(newEl);
 }
 
-//given a number and an object that looks like this:
-// {3: "Fizz", 5:"Buzz", 7:"Boom"}
+// Given a number and an object that looks like this:
+// {3: "Fizz", 5: "Buzz", 7: "Boom"}
 // Loops over the numbers and outputs the number and the matching text for factors
 
 function fizzBuzzBoom(maxNums, factorObj) {
-    //Iterate over all of out numbers
-    for (var num=0; num<maxNums; num++){
-        //reset output string
+    // Iterate over all of out numbers
+    for (var num=0; num<maxNums; num++) {
+        // Reset output string
         var outputStr = "";
         // Iterate over the factors we got from the HTML
-         for (var factor in factorObj) {
-         // Check to see if this num is a multiple of factor
+        for (var factor in factorObj) {
+            // Check to see if this num is a multiple of factor
             if (num % factor == 0) {
-            // If yes, then add the text to the output string
+                // If yes, then add the text to the output string
                 outputStr += factorObj[factor];
             }
         }
-        //now if we have words in outputstr, format it like this "-Fizz!"
+        // Now if we have words in outputStr, format it like this " - Fizz!"
         if (outputStr) {
             outputStr = " - " + outputStr + "!";
         }
         outputToPage(num.toString() + outputStr)
-
+        
     }
 }
 
-//error code
+// Error Code when not all values needed are inputted 
 function reportError(str) {
-    outputEl.html = "<div class='error'" + str + "</div>";
-
+    outputEl.html = "<div class='error'>" + str + "</div>";
 }
 
-//Click listener
+// Click listener 
 $("#submit").click(function() {
     var max = $("#max").val();
     console.log("max:", max)
@@ -69,15 +68,16 @@ $("#submit").click(function() {
         reportError("Please provide a maximum");
         return;
     }
-    
+
     var factorObj = getFactorObj();
     console.log("factorObj:", factorObj);
     if (Object.keys(factorObj).length === 0) {
         reportError("Please provide at least one factor and text");
         return;
     }
-    // clear error 
+    // Clear error if there is one
     outputEl.html = "";
     fizzBuzzBoom(max, factorObj);
     outputEl.addClass("cols");
 });
+
